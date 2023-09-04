2fbc713d8b6b7b02703f96683311163e25e7be82
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-09-04 11:28:22 +0200

    Interfaces reminder

--- tester-collections/src/main/java/com/tester/collections/interfaces/InterfacesDemo.java
+++ tester-collections/src/main/java/com/tester/collections/interfaces/InterfacesDemo.java
@@ -1,13 +1,20 @@
 package com.tester.collections.interfaces;
 
 public class InterfacesDemo {
+
     public static void main(String[] args) {
         Square square = new Square(10.0);
-        System.out.println(square.getArea());
-        System.out.println(square.getPerimeter());
+        showShapeDetails(square);
 
         Circle circle = new Circle(7.0);
-        System.out.println(circle.getArea());
-        System.out.println(circle.getPerimeter());
+        showShapeDetails(circle);
+
+        Triangle triangle = new Triangle(6.0, 4.0, 7.211);
+        showShapeDetails(triangle);
+    }
+
+    public static void showShapeDetails(Shape shape) {
+        System.out.println(shape.getArea());
+        System.out.println(shape.getPerimeter());
     }
 }
