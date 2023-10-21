acee11dff86218526a042f74126161027911bcee
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-10-21 19:38:30 +0200

    Optional example

--- tester-stream/src/main/java/com/tester/optional/OptionalExample.java
+++ tester-stream/src/main/java/com/tester/optional/OptionalExample.java
@@ -7,8 +7,13 @@ import java.util.Optional;
 public class OptionalExample {
 
     public static void main(String[] args) {
-        User user = new User("Jackson", 25, 1200, "Test");
+        User user = new User("UserExampleName", 24, 1200, "ExampleGroup");
 
         Optional<User> optionalUser = Optional.ofNullable(user);
+
+        optionalUser.ifPresent(u -> System.out.println(u.getUsername()));
+        optionalUser.ifPresent(u -> System.out.println(u.getAge()));
+
+
     }
 }
