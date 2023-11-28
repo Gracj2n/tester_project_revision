a422e91daf63bfda8c9973a317f7d5b259001b68
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2023-11-28 16:35:38 +0100

    Parametrized tests tesks

---
+++ tester-advanced-tests/src/test/java/com/tester/parametrized_tests/homework/UserValidatorTest.java
@@ -0,0 +1,35 @@
+package com.tester.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserValidatorTest {
+
+    UserValidator userValidator = new UserValidator();
+
+    //UserNames
+
+    @ParameterizedTest
+    @CsvSource(value = {"AbCd10-._", "333", "..."})
+    void shouldReturnTrueWhenUsernameStringIsCorrect(String input) {
+        assertTrue(userValidator.validateUsername(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"A2", "abC32#", "__"})
+    void shouldReturnFalseWhenUsernameStringIsIncorrect(String input) {
+        assertFalse(userValidator.validateUsername(input));
+    }
+
+
+    //E-mail addresses
+
+    @ParameterizedTest
+    @CsvSource(value = {"abbcde@gmail.com", "123123@gmail.com", "emailexample3@emgi.pl"})
+    void shouldReturnTrueWhenEmailStringIsCorrect(String input) {
+        assertTrue(userValidator.validateEmail(input));
+    }
+
+}
