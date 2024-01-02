5feb3729f573b41877ceb4473001e98c8018d138
Gracjan Chodorski <chodorski.gracjan@gmail.com>
2024-01-02 12:36:23 +0100

    changes 2.01.2024

--- tester-stream/src/main/java/com/tester/exception/FirstExample.java
+++ tester-stream/src/main/java/com/tester/exception/FirstExample.java
@@ -2,9 +2,10 @@ package com.tester.exception;
 
 public class FirstExample {
     public static void main(String[] args) {
-        String username = "null";
+        String username = "John Max 36";
         String[] nameParts = username.split(" ");
         System.out.println("Firstname: " + nameParts[0]);
         System.out.println("Lastname: " + nameParts[1]);
+        System.out.println("Age: " + nameParts[2]);
     }
 }
